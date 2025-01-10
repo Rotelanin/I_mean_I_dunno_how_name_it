633e961d5cba48cbc77e9d9b16e1210ce221ec46
Raddi <141958318+Raddi1@users.noreply.github.com>
2025-01-10 22:40:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-pass
+from fastapi import FastAPI, HTTPException
+from models import User, Team, Match, Result
+
+app = FastAPI()
+
+@app.post("/register", summary="Реєстрація користувача")
+def reg_user():
+    pass
+
+@app.post("/team", summary="Команда")
+def reg_team():
+    pass
+
+@app.get("/match", summary="Турнір")
+def add_match():
+    pass
+
+@app.get("/users", summary="Список користувачів")
+def get_users():
+    pass
+
+@app.get("/teams", summary="Список команд")
+def get_teams():
+    pass
